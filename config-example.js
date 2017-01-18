58cc06adb8633569110b25f72eb6797af2a5ccf4
Ian Gibbs <realflash.uk@googlemail.com>
2017-01-18 11:20:17 +0000

    Switch using the emoji as the command word by default - shows up in autocomplete

--- config-example.js
+++ config-example.js
@@ -4,7 +4,7 @@ var config = {
     bot_name: "WebexBot",									// The name to post the URL as
     token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX',	// Get a test token from https://api.slack.com/docs/oauth-test-tokens
     webex_url: "https://<mysite>.webex.com/meet/", 			// Replace <mysite> with your WebEx site
-    pattern: /^\:webex(.*)/,			 					// The pattern to look for before responding. Default is :webex
+    pattern: /^\:webex:(.*)/,			 					// The pattern to look for before responding. Default is :webex:
 															// The username is expected to follow this
 	username_pattern: /^[\w\.]+$/,							// A regex which should match allowed username formats.
 															// This one is "any number of word characters (a-z, A-Z, 0-9, _) or full stops in any order"
