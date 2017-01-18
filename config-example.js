e076157f0239e78c4e8963f0e3c6c5248c76ea7f
Ian Gibbs <realflash.uk@googlemail.com>
2017-01-18 10:50:19 +0000

    Separate patterns for command word and username, so that a help message can be printed

--- config-example.js
+++ config-example.js
@@ -4,7 +4,12 @@ var config = {
     bot_name: "WebexBot",									// The name to post the URL as
     token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX',	// Get a test token from https://api.slack.com/docs/oauth-test-tokens
     webex_url: "https://<mysite>.webex.com/meet/", 			// Replace <mysite> with your WebEx site
-    pattern: /^\:webex\s+([\w\.]+)\s*$/, 					// The pattern to look for before responding. Default is :webex <username>
+    pattern: /^\:webex(.*)/,			 					// The pattern to look for before responding. Default is :webex
+															// The username is expected to follow this
+	username_pattern: /^[\w\.]+$/,							// A regex which should match allowed username formats.
+															// This one is "any number of word characters (a-z, A-Z, 0-9, _) or full stops in any order"
+	help_message: ":webex <webex-username> e.g. :webex john.smith",	//	What to print when the user gets it wrong. Update this if you 
+															// change pattern
     verbose: true,											// Whether not to be verbose on stdout
     emoji: ":webex:"										//NOTE: you'll need to add this emoji
 
